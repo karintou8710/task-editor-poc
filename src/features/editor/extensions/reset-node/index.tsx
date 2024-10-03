d692121a72a0fa2be0c12e26f7ae46faa3b39c3f
karintou8710 <karintou8710@gmail.com>
2024-10-03 15:57:23 +0900

    feat: ブロックの先頭でEnter時にノード初期化

--- src/features/editor/extensions/reset-node/index.tsx
+++ src/features/editor/extensions/reset-node/index.tsx
@@ -13,8 +13,25 @@ const ResetNode = Extension.create({
           return false;
 
         const $pos = this.editor.$pos(selection.from);
-        // ブロックの先頭で削除か
+        // 単一の選択・ブロックの先頭で削除
         if (!selection.empty || $pos.from !== selection.from) return false;
+
+        return this.editor.commands.setParagraph();
+      },
+
+      Enter: () => {
+        const { selection } = this.editor.state;
+        if (!targetNodes.includes(selection.$from.node().type.name))
+          return false;
+
+        const $pos = this.editor.$pos(selection.from);
+        // 単一の選択・ブロックの先頭・コンテンツ無し
+        if (
+          !selection.empty ||
+          $pos.from !== selection.from ||
+          $pos.node.content.size > 0
+        )
+          return false;
 
         return this.editor.commands.setParagraph();
       },
